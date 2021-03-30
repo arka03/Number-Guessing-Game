2dbd3ba18dd76d6928adee28395f22bcad0b34b2
Arka Banerjee <arka225@gmail.com>
2021-03-31 01:08:42 +0530

    Duplicate Code removed by refactoring

--- script.js
+++ script.js
@@ -7,29 +7,44 @@ document.querySelector('.score').textContent = 10;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
-
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   //for no input value
   if (!guess) {
-    document.querySelector('.message').textContent = 'Enter a Value';
+    displayMessage('Enter a Value');
     document.querySelector('.guess').value = '';
   } else if (guess > 20) {
-    document.querySelector('.message').textContent = 'Range Exceeded';
+    displayMessage('Range Exceeded');
     document.querySelector('.guess').value = '';
 
     //for perfect match
   } else if (guess === secretNumber) {
     document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('.message').textContent = 'Perfect match';
+    displayMessage('Perfect match');
     document.querySelector('body').style.backgroundColor = 'green';
     document.querySelector('.number').style.width = '30rem';
     if (highscore < score) {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
+  } else if (guess !== secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? 'Too High' : 'Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.guess').value = '';
+    } else {
+      document.querySelector('.message').textContent = 'You Lost';
+      document.querySelector('.guess').value = '';
+    }
   }
+});
+/* 
   //for guess is too high
   else if (guess > secretNumber) {
     if (score > 0) {
@@ -53,8 +68,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You Lost';
       document.querySelector('.guess').value = '';
     }
-  }
-});
+  } */
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
