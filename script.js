1e63bf3226f9347117846145f9116eb699891ae5
anarchymonkey <Aniketbatabyal1996@gmail.com>
2021-04-01 14:46:36 +0530

    refactored code for a more modular view

--- script.js
+++ script.js
@@ -4,65 +4,106 @@ document.querySelector('.message').textContent = 'Correct Number';
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
  */
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
-let highscore = 0;
+// let secretNumber = Math.trunc(Math.random() * 20) + 1;
+// let score = 20;
+// let highscore = 0;
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
+let initialValues = {
+  secretNumber: (Math.trunc(Math.random() * 20) + 1),
+  score: 20,
+  highscore: 0,
+}
+/**
+ * 
+ * @param {string} className 
+ * @returns {Element}
+ */
+const selectClassName = (className) => {
+  return document.querySelector(className);
+};
 
-  //for no input value
-  if (!guess) {
-    document.querySelector('.message').textContent = 'Enter a Value';
-    document.querySelector('.guess').value = '';
-  } else if (guess > 20) {
-    document.querySelector('.message').textContent = 'Range Exceeded';
-    document.querySelector('.guess').value = '';
+const classSelectors = {
+  check: selectClassName('.check'),
+  message: selectClassName('.message'),
+  guess: selectClassName('.guess'),
+  number: selectClassName('.number'),
+  body: selectClassName('body'),
+  score: selectClassName('.score'),
+  highscore: selectClassName('.highscore'),
+  again: selectClassName('.again')
 
-    //for perfect match
-  } else if (guess === secretNumber) {
-    document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('.message').textContent = 'Perfect match';
-    document.querySelector('body').style.backgroundColor = 'green';
-    document.querySelector('.number').style.width = '30rem';
-    if (highscore < score) {
-      highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
+}
+
+/**
+ * @param {void}
+ */
+
+const checkCurrentSubmissionScore = () => {
+  classSelectors.check.addEventListener('click', () => {
+    const guess = Number(classSelectors.guess.value);
+    if (!guess) {
+      classSelectors.message.textContent = 'Enter a Value';
+      classSelectors.guess.value = '';
+    } else if (guess > 20) {
+      classSelectors.message.textContent = 'Range Exceeded';
+      classSelectors.guess.value = '';
+
+      //for perfect match
+    } else if (guess === initialValues.secretNumber) {
+      classSelectors.number.textContent = initialValues.secretNumber;
+      classSelectors.message.textContent = 'Perfect match';
+      classSelectors.body.style.backgroundColor = 'green';
+      classSelectors.number.style.width = '30rem';
+      if (initialValues.highscore < initialValues.score) {
+        initialValues = { ...initialValues, highscore: initialValues.score };
+        classSelectors.highscore.textContent = initialValues.highscore;
+      }
     }
-  }
-  //for guess is too high
-  else if (guess > secretNumber) {
-    if (score > 0) {
-      document.querySelector('.message').textContent = 'Too High';
-      score--;
-      document.querySelector('.score').textContent = score;
-      document.querySelector('.guess').value = '';
-    } else {
-      document.querySelector('.message').textContent = 'You Lost';
-      document.querySelector('.guess').value = '';
+    //for guess is too high
+    else if (guess > initialValues.secretNumber) {
+      if (initialValues.score > 0) {
+        classSelectors.message.textContent = 'Too High';
+        const decrementedScore = initialValues.score - 1;
+        initialValues = { ...initialValues, score: decrementedScore }
+        classSelectors.score.textContent = initialValues.score;
+        classSelectors.guess.value = '';
+      } else {
+        classSelectors.message.textContent = 'You Lost';
+        classSelectors.guess.value = '';
+      }
     }
-  }
-  //for guess is too low
-  else if (guess < secretNumber) {
-    if (score > 0) {
-      document.querySelector('.message').textContent = 'Too Low';
-      score--;
-      document.querySelector('.score').textContent = score;
-      document.querySelector('.guess').value = '';
-    } else {
-      document.querySelector('.message').textContent = 'You Lost';
-      document.querySelector('.guess').value = '';
+    //for guess is too low
+    else if (guess < initialValues.secretNumber) {
+      if (initialValues.score > 0) {
+        classSelectors.message.textContent = 'Too Low';
+        const decrementedScore = initialValues.score - 1;
+        initialValues = { ...initialValues, score: decrementedScore }
+        classSelectors.score.textContent = initialValues.score;
+        classSelectors.score.value = '';
+      } else {
+        classSelectors.message.textContent = 'You Lost';
+        classSelectors.guess.value = '';
+      }
     }
-  }
-});
+  });
+};
 
-document.querySelector('.again').addEventListener('click', function () {
-  score = 20;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.score').textContent = '20';
-  document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
-  document.querySelector('.guess').value = '';
-});
+
+const restartGame = () => {
+  classSelectors.again.addEventListener('click', () => {
+    initialValues = { ...initialValues, secretNumber: Math.trunc(Math.random() * 20) + 1, score: 20 };
+    classSelectors.number.textContent = '?';
+    classSelectors.score.textContent = '20';
+    classSelectors.message.textContent = 'Start guessing...';
+    classSelectors.body.setAttribute('style', 'background-color: #FFFFF;');
+    classSelectors.number.style.width = '15rem';
+    classSelectors.guess.value = '';
+  });
+};
+
+
+
+(() => {
+  checkCurrentSubmissionScore();
+  restartGame();
+})();
